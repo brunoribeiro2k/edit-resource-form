6b9be2c071912928ba0d86848f2acaf1f53290ef
brunoribeiro2k <bruno.ribeiro.2k@outlook.com>
2022-06-14 23:00:40 +0100

    Ingredients presented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,37 +5,73 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   capabilityForm: FormGroup;
+  recipeForm: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private http: HttpClient,
-  ) {}
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get('/assets/data.json')
-      .subscribe((data: any[]) => {
-        this.capabilityForm = this.fb.group({
-          capabilities: this.fb.array(data.map(datum => this.generateDatumFormGroup(datum)))
-        });
-      });
+    // this.loadSkillData('/assets/data.json');
+    this.loadRecipeData('/assets/recipeData.json', 0);
   }
 
-  enableSection(index, disabled) {
-    const capabilityFormGroup = (<FormArray>this.capabilityForm.get('capabilities')).at(index);
-    disabled ? capabilityFormGroup.enable() : capabilityFormGroup.disable();
+  private loadRecipeData(path: string, id: number) {
+    this.http.get(path).subscribe((data: any[]) => {
+      this.recipeForm = this.generateRecipeFormGroup(data[id]);
+      console.log('data[id]');
+      console.log(data[id]);
+      console.log('this.recipeForm');
+      console.log(this.recipeForm);
+    });
+  }
+
+  private generateRecipeFormGroup(recipe) {
+    return this.fb.group({
+      title: recipe.title,
+      difficulty: recipe.difficulty,
+      serving: recipe.serving,
+      cookingTime: recipe.cookingTime,
+      preparation: recipe.preparation,
+      ingredients: this.fb.array(
+        recipe.ingredients.map((i) => this.fb.group({ name: i.name }))
+      ),
+      categories: this.fb.array(
+        recipe.categories.map((c) => {
+          this.fb.group({ name: c.name });
+        })
+      ),
+    });
+  }
+
+  private loadSkillData(path: string) {
+    this.http.get(path).subscribe((data: any[]) => {
+      this.capabilityForm = this.fb.group({
+        capabilities: this.fb.array(
+          data.map((datum) => this.generateDatumFormGroup(datum))
+        ),
+      });
+    });
   }
 
   private generateDatumFormGroup(datum) {
     return this.fb.group({
       capability: this.fb.control({ value: datum.Capability, disabled: true }),
-      subCategory: this.fb.control({ value: datum.SubCategory, disabled: true }),
+      subCategory: this.fb.control({
+        value: datum.SubCategory,
+        disabled: true,
+      }),
       skill: this.fb.control({ value: datum.Skill, disabled: true }),
-      skillId: this.fb.control({ value: datum.SkillID, disabled: true })
+      skillId: this.fb.control({ value: datum.SkillID, disabled: true }),
     });
   }
 
+  enableSection(index, disabled) {
+    const capabilityFormGroup = (<FormArray>(
+      this.capabilityForm.get('capabilities')
+    )).at(index);
+    disabled ? capabilityFormGroup.enable() : capabilityFormGroup.disable();
+  }
 }
